32c014fdce7253c8a9714f16f4706d94f9fea81b
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-09-24 20:31:36 +0530

    Data Validation

--- My-App/src/main/java/lk/ijse/dep11/MainFormController.java
+++ My-App/src/main/java/lk/ijse/dep11/MainFormController.java
@@ -5,6 +5,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.dep11.tm.Employer;
+
+import java.util.List;
 
 public class MainFormController {
 
@@ -27,6 +30,27 @@ public class MainFormController {
     }
 
     public void btnDeleteOnACtion(ActionEvent actionEvent) {
+    }
+    private List<Employer> getEmployee(){
+        return tblCustomer.getItems();
+    }
+    public boolean isDataValid(){
+        if(!txtName.getText().strip().matches("^[A-Za-z ]+$")) {
+            txtName.requestFocus();
+            txtName.selectAll();
+            return false;
+        } else if (!txtContact.getText().strip().matches("0\\d{2}-\\d{7}")) {
+            txtContact.requestFocus();
+            txtContact.selectAll();
+            return false;
+        }
+        return true;
+    }
+    private String getNewEmployeeId(){
+        if(getEmployee().isEmpty()) return "E-001";
+        String lastId = getEmployee().get(getEmployee().size()-1).getId();
+        int id = Integer.parseInt(lastId.substring(2))+1;
+        return String.format("E-%03d",id);
     }
 
 }
